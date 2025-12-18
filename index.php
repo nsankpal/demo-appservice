2e7564c798afd9da4a45ba77f0b0b3adcd25acb2
nsankpal <nsankpal@gmail.com>
2025-12-18 16:54:06 +0530

    Add initial HTML structure to index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Neoskills Azure App Service</title>
+</head>
+<body>
+    <h1>Hi, this is dev version of app for Azure App Service!!!.</h1>
+</body>
+</html>
