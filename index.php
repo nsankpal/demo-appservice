2aca50f69cdcccd3042b5f66bac65353e91c95e3
nsankpal <nsankpal@gmail.com>
2025-12-18 17:20:43 +0530

    Change heading to indicate production version

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
     <title>Neoskills Azure App Service</title>
 </head>
 <body>
-    <h1>Hi, this is dev version of app for Azure App Service!!!.</h1>
+    <h1>Hi, this is prod version of app for Azure App Service!!!.</h1>
 </body>
 </html>
